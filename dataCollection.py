d71f2cbcf17754587333f367de8e112274921839
Paul Chibueze <chibuezedevloper@gmail.com>
2024-09-30 19:31:08 +0100

    update

--- dataCollection.py
+++ dataCollection.py
@@ -1,9 +1,12 @@
 import cv2
+from cvzone.HandTrackingModule import HandDetector
 
-capure = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(0)
+detector = HandDetector( maxHands=1)
 
 while True:
-    ret, frame = capure.read()
+    ret, frame = capture.read()
+    hands = detector.findHands(frame)
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
